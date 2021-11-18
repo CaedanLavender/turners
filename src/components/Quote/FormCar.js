5d3125d313883025973576b10aea95bd27a44d0a
Caedan <caedanlavender@gmail.com>
2021-11-19 11:17:53 +1300

    Restructured driver sections

--- src/components/Quote/FormCar.js
+++ src/components/Quote/FormCar.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import 'styles/Quote.css'
+import ToggleSwitch from "components/ToggleSwitch";
 
 const FormCar = () => {
 	const driverTemplate = {
@@ -12,7 +13,7 @@ const FormCar = () => {
 
 	const [carFormData, setCarFormData] = useState({
 		registration: "",
-		business: false,
+		business: "n",
 		address: "",
 		startDate: "",
 	});
@@ -22,26 +23,85 @@ const FormCar = () => {
 		setDriverFormData([...driverFormData, driverTemplate])
 	}
 
+	const removeDriver = (index) => {
+		const newArray = driverFormData.filter((driver, i) => i !== index)
+		setDriverFormData(newArray)
+	
+	}
+
+	const setBusiness = (value) => {
+		setCarFormData({ ...carFormData, business: value })
+	}
+
 	return (
 		<>
 			<h1 className="form">Car</h1>
 			<div className='formSection'>
 				<label>registration of car
-					<input type="text"
-						value={carFormData.registration}
-						onChange={(e) => setCarFormData({ ...carFormData, registration: e.target.value })}
-					/>
+					<div className='formSection__right'>
+						<input type="text"
+							value={carFormData.registration}
+							onChange={(e) => setCarFormData({ ...carFormData, registration: e.target.value })}
+						/>
+					</div>
 				</label>
 			</div>
+
 			<div className='formSection'>
 				<label>used for business
-					<input type="text"
-						value={carFormData.registration}
-						onChange={(e) => setCarFormData({ ...carFormData, registration: e.target.value })}
-					/>
+					<div className='formSection__right'>
+						<ToggleSwitch switches={["y", "n"]} activeSwitch={carFormData.business} setSwitch={setBusiness} />
+					</div>
 				</label>
 			</div>
+
+			<div className='formSection'>
+				<label>address
+					<div className='formSection__right'>
+						<input type="text"
+							value={carFormData.address}
+							onChange={(e) => setCarFormData({ ...carFormData, address: e.target.value })}
+						/>
+					</div>
+				</label>
+			</div>
+
+			<div className='formSection'>
+				<label>start date
+					<div className='formSection__right'>
+						<input type="text"
+							value={carFormData.startDate}
+							onChange={(e) => setCarFormData({ ...carFormData, startDate: e.target.value })}
+						/>
+					</div>
+				</label>
+			</div>
+
+			{
+				driverFormData.map((driver, i) => (
+					<>
+						<h1>Driver {driverFormData.length > 1 && i + 1 }</h1>
+
+						<div className='formSection'>
+							<label>date of birth
+								<div className='formSection__right'>
+									<input type="text"
+										value={driverFormData.dob}
+										onChange={
+											(e) => {
+											setDriverFormData(driverFormData.map((driverr, ii) => ii === i ? {...driverr, dob: e.target.value} : driverr
+										))}}
+									/>
+								</div>
+							</label>
+						</div>
+
+						<button onClick={addDriver}>Add Driver</button>
+					</>
+				))
+			}
 		</>
+
 	)
 }
 
