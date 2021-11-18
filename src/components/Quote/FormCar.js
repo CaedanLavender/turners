a911d4740c4412c26c05aaf402102de6e0900f22
Caedan <caedanlavender@gmail.com>
2021-11-19 09:10:18 +1300

    Added form object and array structure -- added addDriver function

--- src/components/Quote/FormCar.js
+++ src/components/Quote/FormCar.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const FormCar = () => {
+	const driverTemplate = {
+		dob: "",
+		gender: "",
+		licenceType: "",
+		licenceTime: "",
+		incident: false,
+	}
+
+	const [carFormData, setCarFormData] = useState({
+			registration: "",
+			business: false,
+			address: "",
+			startDate: "",
+	});
+	const [driverFormData, setDriverFormData] = useState([driverTemplate]);
+
+	const addDriver = () => {
+		setDriverFormData([...driverFormData, driverTemplate])
+	}
+
 	return (
-		<h1 className="form">Car</h1>
+		<>
+			<h1 className="form">Car</h1>
+			<label>Hey</label>
+		<input type="text" value="testing" />
+		</>
 	)
 }
 
