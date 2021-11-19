fcaa431a9ab06fdfbb1c9b30dba123b47d6e69ff
Caedan <caedanlavender@gmail.com>
2021-11-19 16:28:36 +1300

    Added licence plate search to form

--- src/components/Quote/FormCar.js
+++ src/components/Quote/FormCar.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useState, useEffect } from "react";
 import 'styles/Quote.css'
 import ToggleSwitch from "components/ToggleSwitch";
@@ -33,6 +34,8 @@ const FormCar = () => {
 		startDate: "",
 	});
 	const [driverFormData, setDriverFormData] = useState([driverTemplate]);
+	const [carData, setCarData] = useState();
+	const [selectedCar, setSelectedCar] = useState("")
 
 	const addDriver = () => {
 		setDriverFormData([...driverFormData, driverTemplate])
@@ -48,29 +51,30 @@ const FormCar = () => {
 		setCarFormData({ ...carFormData, business: value })
 	}
 
-	// const setGender = (value, driver) => {
-	// 	console.log(value)
-	// 	setDriverFormData(
-	// 		() => {
-	// 		console.log(driverFormData.map((arrayItem, i) => driver === i ? {...arrayItem, gender: value} : arrayItem
-	// 	))}
-	// 	)
-	// }
-
 	const setGender = (value) => {
 		setDriverFormData(driverFormData.map((driver, i) => i === 0 ? ({ ...driver, gender: value }) : driver))
 	}
-	
+
 	const setLicence = (value) => {
 		setDriverFormData(driverFormData.map((driver, i) => i === 0 ? ({ ...driver, licenceType: value }) : driver))
 	}
-	
+
 	const setLicenceTime = (value) => {
 		setDriverFormData(driverFormData.map((driver, i) => i === 0 ? ({ ...driver, licenceTime: value }) : driver))
 	}
-	
+
 	const setIncident = (value) => {
 		setDriverFormData(driverFormData.map((driver, i) => i === 0 ? ({ ...driver, incident: value }) : driver))
+	}
+
+	const getCarData = () => {
+		axios.get('http://localhost:4000/plate', {
+			params: {
+				plate: carFormData.registration
+			}
+		})
+		.then(res => setCarData(res.data))
+		.catch(() => console.log("There was a catch error"))
 	}
 
 
@@ -81,9 +85,17 @@ const FormCar = () => {
 				<label><div className='labelText'>registration of car</div>
 					<div className='formSection__right'>
 						<input type="text"
+							placeholder="e.g. ABC123"
 							value={carFormData.registration}
 							onChange={(e) => setCarFormData({ ...carFormData, registration: e.target.value })}
 						/>
+						<div className='searchButton' onClick={getCarData}>Search</div>
+						<div className='radioOutput'>
+							<div className={`carDataLabel ${carData || "hidden"}`}>
+								<input type='radio' checked={carData === selectedCar} onChange={() => setSelectedCar(carData)} id='carSelect' value={carData}/>
+								{carData}
+							</div>
+						</div>
 					</div>
 				</label>
 			</div>
@@ -97,9 +109,10 @@ const FormCar = () => {
 			</div>
 
 			<div className='formSection'>
-				<label>address
+				<label><div className="labelText">address</div>
 					<div className='formSection__right'>
 						<input type="text"
+							placeholder="e.g. 123 Smith St"
 							value={carFormData.address}
 							onChange={(e) => setCarFormData({ ...carFormData, address: e.target.value })}
 						/>
@@ -111,6 +124,7 @@ const FormCar = () => {
 				<label>start date
 					<div className='formSection__right'>
 						<input type="text"
+							placeholder="DD / MM / YYYY"
 							value={carFormData.startDate}
 							onChange={(e) => setCarFormData({ ...carFormData, startDate: e.target.value })}
 						/>
@@ -127,6 +141,7 @@ const FormCar = () => {
 							<label><div className="labelText">date of birth</div>
 								<div className='formSection__right'>
 									<input type="text"
+										placeholder="DD / MM / YYYY"
 										value={driverFormData.dob}
 										onChange={
 											(e) => {
@@ -141,27 +156,27 @@ const FormCar = () => {
 						<div className='formSection'>
 							<label><div className="labelText">gender</div>
 								<div className='formSection__right'>
-									<ToggleSwitch switches={["m", "f"]} activeSwitch={driverFormData[0].gender} setSwitch={setGender}/>
+									<ToggleSwitch switches={["m", "f"]} activeSwitch={driverFormData[0].gender} setSwitch={setGender} />
 								</div>
 							</label>
 						</div>
 
 						<div className='formSection'>
 							<label><div className="labelText">type of licence</div>
-								<SelectList list={typeOfLicence} activeItem={driverFormData[0].licenceType} setItem={setLicence}/>
+								<SelectList list={typeOfLicence} activeItem={driverFormData[0].licenceType} setItem={setLicence} />
 							</label>
 						</div>
 
 						<div className='formSection'>
 							<label><div className="labelText">time you've had your licence</div>
-								<SelectList list={timeOnLicence} activeItem={driverFormData[0].licenceTime} setItem={setLicenceTime}/>
+								<SelectList list={timeOnLicence} activeItem={driverFormData[0].licenceTime} setItem={setLicenceTime} />
 							</label>
 						</div>
 
 						<div className='formSection'>
 							<label><div className="labelText">had a claim/incident(s) in the last 5 years?</div>
 								<div className='formSection__right'>
-									<ToggleSwitch switches={["y", "n"]} activeSwitch={driverFormData[0].incident} setSwitch={setIncident}/>
+									<ToggleSwitch switches={["y", "n"]} activeSwitch={driverFormData[0].incident} setSwitch={setIncident} />
 								</div>
 							</label>
 						</div>
