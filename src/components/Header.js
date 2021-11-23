fc1766235923cbfb33b71b48c74f844c5156d1b5
keanu teina <keanut@missionreadyhq.com>
2021-11-24 01:26:21 +1300

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,49 @@
 import React from "react";
+import './Header.css';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import AddLocationIcon from '@mui/icons-material/AddLocation';
 
 const Header = () => {
 	return (
-		<h1>Header content goes here</h1>
+		<div>
+		<div classNamee="header-top">
+			<div className="header-options">
+			<button type="button" className="header-options-text" onClick="">Cars</button>
+			<button type="button" className="header-options-text" onClick="">Trucks &amp; machinery </button>
+			<button type="button" className="header-options-text" onClick="">Damaged &amp; end of lide</button>
+			<button type="button" className="header-options-text" onClick="">Boats &amp; marine</button>
+			<button type="button" className="header-options-text" onClick="">Motorcycles &amp; scooters</button>
+			<button type="button" className="header-options-text" onClick="">General goods</button>
+			<button type="button" className="header-options-text" onClick="">Buses, caravans &amp; motorhomes</button>
+			</div>
+			<div className="header-info">
+			<div className="header-info-left">
+			<img src="turners-logo.jpg" className="footer-pic" alt=""/></div>
+			<div className="header-info-right">
+			<button type="button" className="header-info-buttons" onClick=""><AccountCircleIcon/> LOGIN or REGISTER</button>
+			<button type="button" className="header-info-buttons" onClick=""><LocalPhoneIcon/> 0800 887 637</button>
+			<button type="button" className="header-info-buttons" onClick=""><AddLocationIcon/> Find Us</button>
+			</div>
+			</div>
+		</div>
+		<div className="navbar">
+			<div className="navbar-leftside">
+				<div className="nav-links">
+				<button type="button" className="header-text" onClick="">Buy</button>
+				<button type="button" className="header-text" onClick="">Sell</button>
+				<button type="button" className="header-text" onClick="">Finance</button>
+				<button type="button" className="header-text" onClick="">Insurance</button>
+				<button type="button" className="header-text" onClick="">Careers</button>
+				<input type="text" placeholder="Search..."/>
+				</div>
+			</div>
+			<div className="navbar-rightside">
+			<button type="button" className="header-right-text" onClick="">Auction &amp; events</button>
+			<button type="button" className="header-right-text" onClick="">Services &amp; information</button>
+			</div>
+		</div>
+		</div>
 	)
 }
 
